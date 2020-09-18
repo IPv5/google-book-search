a1048bff0df08d29e1bcf1054ccc68c42600e1da
IPv5 <donationbuycraft@gmail>
2020-09-17 18:15:50 -0700

    another heroku

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.use(express.json());
 //     useNewUrlParser: true,
 //     useUnifiedTopology: true
 //   });
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+}
 
 mongoose.connection.on('connected', function () {
   console.log("Connected to mongodb")
@@ -27,10 +31,9 @@ app.listen(PORT, () => {
 // Add routes, both API and view
 app.use(routes);
 
-app.use(express.static(path.join(__dirname, '../build')))
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build'))
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 // // Serve static assets in production
 // if (process.env.NODE_ENV === "production") {
